a75bb91233350273131d1cf38a8856bf9cd6bea9
SheetalVishwakarma <000sheetal@gmail.com>
2015-05-13 12:43:48 +0530

    home page

---
+++ home page/home.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<form>
+<table>
+    <tr><td colspan="3"><input type="button" value="injured animal" name="ia"></td></tr>
+    <tr><td><input type="button" value="FAQ" name="faq"></td>
+    <</tr>
+    
+</table>
+</form>
